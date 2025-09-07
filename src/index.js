7f17694daab3153512c9f04d70af626ac5054311
HoneySafonovets <kfs.boi@yandex.ru>
2025-09-07 17:32:38 +0300

    feat: add solution for task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
-module.exports = function towelSort(/* matrix */) {
-  throw new Error('Not implemented');
+module.exports = function towelSort(matrix) {
+  if (matrix === undefined) {
+    return [];
+  }
+  if (matrix.length > 0 || matrix === '') {
+    for (let i = 0; i < matrix.length; i += 1) {
+      if (i % 2 !== 0) {
+        matrix[i].reverse();
+      }
+    }
+    return matrix.flat();
+  }
+  return [];
 };
